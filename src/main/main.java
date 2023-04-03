bdbfc9b420eb01cbcb672617a1a0b4704f2508b9
Juan Alegre <jpalegree@gmail.com>
2023-04-02 21:54:44 -0300

    Initial commit

---
+++ src/main/main.java
@@ -0,0 +1,19 @@
+package main;
+
+
+
+public class main {
+
+    public static void main(String[] args) {
+        String dato1 = "Hola mundo";
+        int dato2 = 2;
+        boolean dato3 = true;
+        long dato4 = 1000000;
+        double dato5 = 10.22;
+        System.out.println(dato1);
+        System.out.println(dato2);
+        System.out.println(dato3);
+        System.out.println(dato4);
+        System.out.println(dato5);
+    }
+}
